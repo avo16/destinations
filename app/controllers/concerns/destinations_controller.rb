aa750f9bf0ef5c7f24b2a1fbb11269ac2dfe5844
Vlad Dialog <vlad.dialog@softvision.ro>
2021-11-06 14:52:01 +0200

    Weather Gem: - add initializer to config api key - add one example on destinations index

--- app/controllers/concerns/destinations_controller.rb
+++ app/controllers/concerns/destinations_controller.rb
@@ -1,5 +1,9 @@
+require 'weather_destination'
+
 class DestinationsController < ApplicationController
 	def index
+		params = { city: 'London'}
+		@weather = WeatherDestination.call('current', params)
 		@destinations = Destination.all
 	end
 end
